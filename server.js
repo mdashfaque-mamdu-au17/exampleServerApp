8d7d0c2b9633221afd591a6cd73cb08119684eff
mdashfaque <ashfaquemamdu666@gmail.com>
2021-10-05 17:56:23 +0530

    correction made

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ app.get("/", (req, res) => {
   res.send("server working this is homepage");
 });
 
-app.listen(4000, () => {
-  console.log("server started at port 3000");
+console.log(`Herokuk port number is ${process.env.PORT}`);
+const prt = process.env.PORT || 4000;
+app.listen(prt, () => {
+  console.log(`server started at ${prt}`);
 });
